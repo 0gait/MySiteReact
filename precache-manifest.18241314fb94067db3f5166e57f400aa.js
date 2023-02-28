1e2f5242d562458a337cfebf30c2a6960abac9cf
Tiago Henriques <91702960+0gait@users.noreply.github.com>
2023-02-28 12:14:09 +0000

    Updates

---
+++ precache-manifest.18241314fb94067db3f5166e57f400aa.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "4eacd8063ef85c34b377",
+    "url": "/MySiteReact/static/js/main.8cd12d61.chunk.js"
+  },
+  {
+    "revision": "dfe2926fd9faed1c99a2",
+    "url": "/MySiteReact/static/js/runtime~main.6f832765.js"
+  },
+  {
+    "revision": "6c6fdffc6944b917a926",
+    "url": "/MySiteReact/static/css/2.9243e05c.chunk.css"
+  },
+  {
+    "revision": "6c6fdffc6944b917a926",
+    "url": "/MySiteReact/static/js/2.8896161b.chunk.js"
+  },
+  {
+    "revision": "7c0272b7d233366adf1d7a1dd34bc45f",
+    "url": "/MySiteReact/static/media/logo.7c0272b7.svg"
+  },
+  {
+    "revision": "a091c76c9b14a992ea5c4f414951565f",
+    "url": "/MySiteReact/static/media/dxicons.a091c76c.woff2"
+  },
+  {
+    "revision": "361e8c85c185560d1223735e93ec6271",
+    "url": "/MySiteReact/static/media/dxicons.361e8c85.woff"
+  },
+  {
+    "revision": "9acd66044b85738abc90e5c630fefad2",
+    "url": "/MySiteReact/static/media/dxicons.9acd6604.ttf"
+  },
+  {
+    "revision": "2d767ec1f991af9be766bd3e7870d3d4",
+    "url": "/MySiteReact/static/media/calendario.2d767ec1.pdf"
+  },
+  {
+    "revision": "a7bc51610cbe0435ecab5eb2860c3433",
+    "url": "/MySiteReact/static/media/horario.a7bc5161.png"
+  },
+  {
+    "revision": "d6c6a09fcfda8247cc90ee1932d9967d",
+    "url": "/MySiteReact/index.html"
+  }
+];
